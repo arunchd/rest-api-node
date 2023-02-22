373b19e77035ee443bdc8fee6a1c275f5c30871a
Arun Singh <72827745+arunchd@users.noreply.github.com>
2023-02-22 17:35:17 +0530

    Update connect.js

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 
-/* uri = "mongodb+srv://arunsingh:HzuW6Q8wvQU3vPu9@restapi.3vsfota.mongodb.net/REST-API?retryWrites=true&w=majority"; */
-
 mongoose.set("strictQuery", false);
 
 const connectDB = (uri)=>{
